14d5d956026cb9632c3cba4986c8db1719d2f777
Jacob T <129910091+glassfox15@users.noreply.github.com>
2025-03-09 12:08:36 -0700

    resolve plotting issues

--- execute_training.py
+++ execute_training.py
@@ -6,7 +6,7 @@ import numpy as np
 
 # Inputs
 checkpoint_path = ''            # load saved agent: 'checkpoint_episode_###.pth'
-episodes = 10                   # number of desired episodes, actual training used around 500 episodes
+episodes = 11                   # number of desired episodes, actual training used around 500 episodes
 save_plots = False              # whether to save plots of results
 
 # NOTE: Checkpoints save every 10 episodes, so files for episode 0 and episode 10 should exist once those episodes complete.
@@ -34,37 +34,37 @@ if __name__ == "__main__":
 
 # ===== PLOTTING =====
 
+fig, ax = plt.subplots(1, 3, figsize=(4*3, 1.5*3))
+
 # Total Episode Score
-fig1, ax1 = plt.subplots()
-ax1.plot(range(len(scores)), scores, label='Score', color='blue')
-ax1.set_xlabel('Episode')
-ax1.set_ylabel('Score')
-ax1.set_title('Score per Episode')
-ax1.legend()
-ax1.grid(True)  # Add grid lines for better readability
+ax[0].plot(range(len(scores)), scores, label='Score', color='blue')
+ax[0].set_xlabel('Episode')
+ax[0].set_ylabel('Score')
+ax[0].set_title('Score per Episode')
+ax[0].legend()
+ax[0].grid(True)  # Add grid lines for better readability
 
 
 # Cumulative Average Score up to Episode
 avg_scores = np.cumsum(scores) / np.arange(1, len(scores) + 1)
 
-fig2, ax2 = plt.subplots()
-ax2.plot(range(len(scores)), avg_scores, label='Average Score', color='green')
-ax2.set_xlabel('Episode')
-ax2.set_ylabel('Score')
-ax2.set_title('Cumulative Average Score up to Episode')
-ax2.legend()
-ax2.grid(True)
+ax[1].plot(range(len(scores)), avg_scores, label='Average Score', color='green')
+ax[1].set_xlabel('Episode')
+ax[1].set_ylabel('Score')
+ax[1].set_title('Cumulative Average Score up to Episode')
+ax[1].legend()
+ax[1].grid(True)
 
 # Maximum Tiles Achieved
-fig3, ax3 = plt.subplots()
-ax3.plot(range(len(scores)), max_tiles, label='Tile Value', color='red')
-ax3.set_yscale('log', base=2)
-ax3.set_xlabel('Episode')
-ax3.set_ylabel('Tile Value')
-ax3.set_title('Maximum Tiles Achieved')
-ax3.legend()
-ax3.grid(True)
+ax[2].plot(range(len(scores)), max_tiles, label='Tile Value', color='red')
+ax[2].set_yscale('log', base=2)
+ax[2].set_xlabel('Episode')
+ax[2].set_ylabel('Tile Value')
+ax[2].set_title('Maximum Tiles Achieved')
+ax[2].legend()
+ax[2].grid(True)
 
 # Display all three graphs
+plt.tight_layout()
 if save_plots: plt.savefig('training_plots.png')
 plt.show()
